4c88e453e73ebcc61cd15228ae78c8683aa4f56a
hamed18 <hamedcnec@gmail.com>
2024-09-03 22:08:34 +0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h3 className="text-center">Food Sharing</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
