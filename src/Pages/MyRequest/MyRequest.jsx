1eb4597d3e4a19227fdd206b808b4bc4752b9374
hamed18 <hamedcnec@gmail.com>
2024-09-04 20:10:52 +0600

    After fetching data by query, filter the data by another parameter foodStatus. My Request UI

--- src/Pages/MyRequest/MyRequest.jsx
+++ src/Pages/MyRequest/MyRequest.jsx
@@ -1,21 +1,25 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
 
 const MyRequest = () => {
 	const {user} = useContext(AuthContext);
+	const [MyRequest,setMyrequest] = useState([]);
 	const url = `http://localhost:3000/availablebyEmail/${user.email}`;
 	useEffect(() => {
 		fetch(url)
 		.then(res => res.json())
 		.then((data) => {
 			console.log('sucess in query',data);
+			const remaining = data.filter(obj => obj.foodStatus === 'requested');  
+			setMyrequest(remaining);
+			console.log(MyRequest);
 		})
 		.catch(err => console.log('error using query', err));
-	} ,[])
+	} ,[user.email])
 
 	return (
 		<div>
-			
+			<h3 className="text-center mt-8 mb-4">Your Requested Food Item is {MyRequest.length}</h3>
 		</div>
 	);
 };
